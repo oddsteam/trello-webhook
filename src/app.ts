c9ba467027659e5c6dc62a6018a77d76365e39f8
thotsaphonjantree <thotsaphon.jantree@gmail.com>
2023-01-26 07:31:27 +0700

    add function generate chart by mock data

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+export const startApplication = () => {
+  const app = express();
+  const port = 7111;
+
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
+    res.send("LeSSeX Trello Webhook App listening");
+  });
+
+  app.head("/callback", (req, res) => {
+    res.status(200).send();
+  });
+  app.post("/callback", (req, res) => {
+    console.log(req.body.action.data);
+    res.status(200).send();
+  });
+
+  app.listen(port, () => {
+    console.log(`LeSSeX Trello Webhook App listening on port ${port}`);
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
-import express from "express";
+import { startApplication } from "./app";
 
-const app = express();
-const port = 7111;
+startApplication()
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("LeSSeX Trello Webhook App listening");
-});
-
-app.head("/callback", (req, res) => {
-  res.status(200).send();
-});
-app.post("/callback", (req, res) => {
-  console.log(req.body.action.data);
-  res.status(200).send();
-});
-
-app.listen(port, () => {
-  console.log(`LeSSeX Trello Webhook App listening on port ${port}`);
-});
+const generateChart = () => {
+    const data = [{ sprint: "2023/01/23 - 2023/01/27", date: "2023/01/23", point: 100 },
+    { sprint: "2023/01/23 - 2023/01/27", date: "2023/01/23", point: 50 },
+    { sprint: "2023/01/23 - 2023/01/27", date: "2023/01/23", point: 70 },
+    { sprint: "2023/01/23 - 2023/01/27", date: "2023/01/23", point: 30 },
+    { sprint: "2023/01/23 - 2023/01/27", date: "2023/01/23", point: 0 },
+    ]
+    const points = data.map(el => el.point).join(",")
+    const labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].join(",")
+    const url = `https://quickchart.io/chart/render/sm-a5acd22b-74e1-4ee8-b291-403011afc5c5?title=Sprint ${data[0].sprint}&labels=${labels}&data1=${points}`
+    console.log(url)
+}
+generateChart()
